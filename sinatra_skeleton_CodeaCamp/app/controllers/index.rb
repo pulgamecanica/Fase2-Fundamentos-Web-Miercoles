c2e2af3a28cf9775883a124b9fa200eca4b3050b
codeamac06 <codea_mac_06@CodeaCamp06s-Mac-mini.local>
2017-07-03 13:45:44 -0500

    Final

--- sinatra_skeleton_CodeaCamp/app/controllers/index.rb
+++ sinatra_skeleton_CodeaCamp/app/controllers/index.rb
@@ -1,7 +1,7 @@
 get '/' do
   # La siguiente linea hace render de la vista 
   # que esta en app/views/index.erb
-  @urls = Url.all.reverse
+  @urls_total = Url.all
   erb :index
 end
 
@@ -10,19 +10,16 @@ get '/:urls' do
   # redirige a la URL original
   url_input = params[:urls]
   url = Url.find_by short: url_input 
-  click_count = Url.visits = visits + 1
-  update = Url.update(visits: click_count)
-  update = save
-
+  visits = url.visits +=1
+  visits.save
   redirect_to url.original
 end
 
 post '/urls' do
   # crea una nueva Url
-  @url = params[:data]
-  new_url = Url.create(original: @url, visits: 0)
-  if newURL.valid?
-    newURL.save
+  @url = params[:user_input]
+  new_URL = Url.create(original: @url, visits: 0)
+  if new_URL.save
     p session[:message] = "Saved"
     else
     p "Error"
